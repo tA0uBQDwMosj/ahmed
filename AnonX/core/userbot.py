ea769a49f07a5630cbfa3cab9291bcadcd8e9bf0
Source Freedom <104025850+SoRsRR8r9@users.noreply.github.com>
2022-10-07 15:48:52 +0300

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("AnonAssociation")
+                await self.one.join_chat("xl444")
+                await self.one.join_chat("vvyvv6")
             except:
                 pass
             assistants.append(1)
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("AnonAssociation")
+                await self.two.join_chat("xl444")
+                await self.two.join_chat("vvyvv6")
             except:
                 pass
             assistants.append(2)
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("AnonAssociation")
+                await self.three.join_chat("xl444")
+                await self.three.join_chat("vvyvv6")
             except:
                 pass
             assistants.append(3)
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("AnonAssociation")
+                await self.four.join_chat("xl444")
+                await self.four.join_chat("vvyvv6")
             except:
                 pass
             assistants.append(4)
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("AnonAssociation")
+                await self.five.join_chat("xl444")
+                await self.five.join_chat("vvyvv6")
             except:
                 pass
             assistants.append(5)
